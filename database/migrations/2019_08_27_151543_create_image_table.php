08c5f2a93890bbc0cf59baf7d9120acb2659519b
Nayrakhaled <sci.nayerakhaled2018@alexu.edu.eg>
2019-08-30 15:21:21 +0200

    imagemigrate

---
+++ database/migrations/2019_08_27_151543_create_image_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('image', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('link');
+            $table->unsignedBigInteger('hotel_id');
+            $table->foreign('hotel_id')->references('id')->on('hotels');
+            $table->enum('type',['main','extra']);
+            $table->string('desc');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('image');
+    }
+}
